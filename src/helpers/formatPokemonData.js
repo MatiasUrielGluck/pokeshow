26eea7fbc64d4c5c057552a671148dd2f1dae2e3
Matias Gluck <matiasugluck@gmail.com>
2024-03-10 02:19:16 -0300

    feat: share data improvement

--- src/helpers/formatPokemonData.js
+++ src/helpers/formatPokemonData.js
@@ -1,7 +1,9 @@
 export const formatPokemonData = (pokeData) => {
-  let result = ''
+  let result = 'Hey! Check out this pokemon...\n\n'
   for (const key of Object.keys(pokeData)) {
     result += `${key}: ${pokeData[key]}\n`
   }
-  return result.slice(0, result.length - 1)
+  result = result.slice(0, result.length - 1)
+  result += '\n\nhttps://pokeshow-matiasurielgluck.netlify.app'
+  return result
 }
